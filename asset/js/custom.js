12e710d1aef9a0b2d73332cfa3204434c0ecbadd
Sabbir-96 <sabbirislam9404@gmail.com>
2024-12-02 22:46:21 +0600

    Add files via upload

---
+++ asset/js/custom.js
@@ -0,0 +1,91 @@
+// companise profile
+
+$(document).ready(function(){
+   
+  $('.responsive').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 600,
+      slidesToShow: 5,
+      slidesToScroll: 4,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+});
+
+// wow part
+
+new WOW().init();
+
+
+
+// product slide part
+
+$(document).ready(function(){
+   
+  $('.o_product').slick({
+      dots: true,
+      infinite: true,
+      autoplay: true,
+      speed: 800,
+      slidesToShow: 6,
+      slidesToScroll: 2,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
+    });
+
+});
